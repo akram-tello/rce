a37ad8ce336ffdbe13199c87362d9e5a6f9b5ab3
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-12-19 22:42:34 +0800

    added auth api callback

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,226 @@
 import Head from 'next/head'
+import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const { user, error, isLoading } = useUser();
+  
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+  if (user) {
+    return (
+      <div>
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+      </div>
+    );
+  }
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+  return <a href="/api/auth/login">Login</a>;
+  
+  // return (
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+    // <div className="container">
+    //   <Head>
+    //     <title>Create Next App</title>
+    //     <link rel="icon" href="/favicon.ico" />
+    //   </Head>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+    //   <main>
+    //     <h1 className="title">
+    //       Welcome to <a href="https://nextjs.org">Next.js!</a>
+    //     </h1>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+    //     <p className="description">
+    //       Get started by editing <code>pages/index.js</code>
+    //     </p>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+    //     <div className="grid">
+    //       <a href="https://nextjs.org/docs" className="card">
+    //         <h3>Documentation &rarr;</h3>
+    //         <p>Find in-depth information about Next.js features and API.</p>
+    //       </a>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
+    //       <a href="https://nextjs.org/learn" className="card">
+    //         <h3>Learn &rarr;</h3>
+    //         <p>Learn about Next.js in an interactive course with quizzes!</p>
+    //       </a>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+    //       <a
+    //         href="https://github.com/vercel/next.js/tree/master/examples"
+    //         className="card"
+    //       >
+    //         <h3>Examples &rarr;</h3>
+    //         <p>Discover and deploy boilerplate example Next.js projects.</p>
+    //       </a>
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+    //       <a
+    //         href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+    //         className="card"
+    //       >
+    //         <h3>Deploy &rarr;</h3>
+    //         <p>
+    //           Instantly deploy your Next.js site to a public URL with Vercel.
+    //         </p>
+    //       </a>
+    //     </div>
+    //   </main>
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+    //   <footer>
+    //     <a
+    //       href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Powered by{' '}
+    //       <img src="/vercel.svg" alt="Vercel" className="logo" />
+    //     </a>
+    //   </footer>
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+    //   <style jsx>{`
+    //     .container {
+    //       min-height: 100vh;
+    //       padding: 0 0.5rem;
+    //       display: flex;
+    //       flex-direction: column;
+    //       justify-content: center;
+    //       align-items: center;
+    //     }
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+    //     main {
+    //       padding: 5rem 0;
+    //       flex: 1;
+    //       display: flex;
+    //       flex-direction: column;
+    //       justify-content: center;
+    //       align-items: center;
+    //     }
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+    //     footer {
+    //       width: 100%;
+    //       height: 100px;
+    //       border-top: 1px solid #eaeaea;
+    //       display: flex;
+    //       justify-content: center;
+    //       align-items: center;
+    //     }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
+    //     footer img {
+    //       margin-left: 0.5rem;
+    //     }
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
+    //     footer a {
+    //       display: flex;
+    //       justify-content: center;
+    //       align-items: center;
+    //     }
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
+    //     a {
+    //       color: inherit;
+    //       text-decoration: none;
+    //     }
 
-        .title,
-        .description {
-          text-align: center;
-        }
+    //     .title a {
+    //       color: #0070f3;
+    //       text-decoration: none;
+    //     }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
+    //     .title a:hover,
+    //     .title a:focus,
+    //     .title a:active {
+    //       text-decoration: underline;
+    //     }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
+    //     .title {
+    //       margin: 0;
+    //       line-height: 1.15;
+    //       font-size: 4rem;
+    //     }
 
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
+    //     .title,
+    //     .description {
+    //       text-align: center;
+    //     }
 
-          max-width: 800px;
-          margin-top: 3rem;
-        }
+    //     .description {
+    //       line-height: 1.5;
+    //       font-size: 1.5rem;
+    //     }
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
+    //     code {
+    //       background: #fafafa;
+    //       border-radius: 5px;
+    //       padding: 0.75rem;
+    //       font-size: 1.1rem;
+    //       font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+    //         DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+    //     }
 
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
+    //     .grid {
+    //       display: flex;
+    //       align-items: center;
+    //       justify-content: center;
+    //       flex-wrap: wrap;
 
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
+    //       max-width: 800px;
+    //       margin-top: 3rem;
+    //     }
 
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
+    //     .card {
+    //       margin: 1rem;
+    //       flex-basis: 45%;
+    //       padding: 1.5rem;
+    //       text-align: left;
+    //       color: inherit;
+    //       text-decoration: none;
+    //       border: 1px solid #eaeaea;
+    //       border-radius: 10px;
+    //       transition: color 0.15s ease, border-color 0.15s ease;
+    //     }
 
-        .logo {
-          height: 1em;
-        }
+    //     .card:hover,
+    //     .card:focus,
+    //     .card:active {
+    //       color: #0070f3;
+    //       border-color: #0070f3;
+    //     }
 
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
+    //     .card h3 {
+    //       margin: 0 0 1rem 0;
+    //       font-size: 1.5rem;
+    //     }
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+    //     .card p {
+    //       margin: 0;
+    //       font-size: 1.25rem;
+    //       line-height: 1.5;
+    //     }
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+    //     .logo {
+    //       height: 1em;
+    //     }
+
+    //     @media (max-width: 600px) {
+    //       .grid {
+    //         width: 100%;
+    //         flex-direction: column;
+    //       }
+    //     }
+    //   `}</style>
+
+    //   <style jsx global>{`
+    //     html,
+    //     body {
+    //       padding: 0;
+    //       margin: 0;
+    //       font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+    //         Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+    //         sans-serif;
+    //     }
+
+    //     * {
+    //       box-sizing: border-box;
+    //     }
+    //   `}</style>
+    // </div>
+  // )
 }
