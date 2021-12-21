0d1ace4ddb58c844cae72d5ce0ad40bfbb80db41
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-12-21 19:11:17 +0800

    store the file uploaded into state

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
 
 export class Home extends Component {
-    fileSelectedHandler = (event) => {
-        console.log(event.target.files[0])
+    state = {   
+        slectedFile: null,
     }
 
+    fileSelectedHandler = (event) => {
+        console.log(event.target.files[0])
+        this.setState({
+            slectedFile: event.target.files[0]
+        })
+    }
+
+    fileUploadHandler = () => {
+    
+    }
     render() {
         return (
             <>
                 <a href="/api/auth/login">Login</a><br /> <br />
                 <input type="file" name="file" id="file" onChange={this.fileSelectedHandler} />
+                <button onClick={this.fileUploadHandler}>Upload</button>
             </>
         )
     }
