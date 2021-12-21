321651e0f02a1608c58165beb21c2483d8d4b771
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-12-21 20:39:46 +0800

    update upload types file

--- components/UploadForm.js
+++ components/UploadForm.js
@@ -5,10 +5,11 @@ const UploadForm = () => {
   const [file, setFile] = useState(null);
   const [error, setError] = useState(null);
 
-  const types = ['image/png', 'image/jpeg'];
+  const types = ['image/png', 'image/jpeg','application/pdf'];
 
   const handleChange = (e) => {
     let selected = e.target.files[0];
+    console.log(selected);
 
     if (selected && types.includes(selected.type)) {
       setFile(selected);
@@ -30,7 +31,7 @@ const UploadForm = () => {
       <div className="output">
         { error && <div className="error">{ error }</div>}
         { file && <div>{ file.name }</div> }
-        { file && <ProgressBar file={file} setFile={setFile} /> }
+        {/* { file && <ProgressBar file={file} setFile={setFile} /> } */}
       </div>
     </form>
       </>
