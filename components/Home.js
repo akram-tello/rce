e6bb8326f699d0a734e507d05c8463a29011b32a
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-12-21 19:06:28 +0800

    added fileSelectedHandler

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 
 export class Home extends Component {
+    fileSelectedHandler = (event) => {
+        console.log(event.target.files[0])
+    }
 
     render() {
         return (
             <>
                 <a href="/api/auth/login">Login</a><br /> <br />
-                <input type="file" name="file" id="file" />
+                <input type="file" name="file" id="file" onChange={this.fileSelectedHandler} />
             </>
         )
     }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { useUser } from '@auth0/nextjs-auth0';
 import HomePage from '../components/Home';
 
