f2962c1da5567b9ccf691853d86f09fd4355325e
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-12-21 20:09:42 +0800

    added global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0';
 import Head from 'next/head';
+import '../styles/global.css';
+import '../styles/reset.css';
 
-// import '../styles/global.scss'
 // import 'antd/dist/antd.css'
 
 export default function App({ Component, pageProps }) {
