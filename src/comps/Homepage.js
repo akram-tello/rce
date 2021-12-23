69640eece75b2145e2583d90a9740048d33cf26a
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-12-23 13:50:53 +0800

    added homepage component

---
+++ src/comps/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+
+const Homepage = () => {
+  return (
+    <div className="Homepage">
+      <h1>Homepage</h1>
+    </div>
+  );
+};
+
+export default Homepage;
