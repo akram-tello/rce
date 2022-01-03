8f896eab96b9c388d0a273450846e25d8cb66843
Akramtello <35160721+akram-tello@users.noreply.github.com>
2022-01-03 13:21:26 +0800

    ..

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import UploadForm from './comps/UploadForm';
 import Homepage from './comps/Homepage';
 import ImageGrid from './comps/ImageGrid';
-import Modal from './comps/Modal';
+// import Modal from './comps/Modal';
 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
         logout,
     } = useAuth0();
 
-    const [selectedImg, setSelectedImg] = useState(null);
+    const [setSelectedImg] = useState(null);
 
     if (isLoading) {
         return <div>Loading...</div>;
